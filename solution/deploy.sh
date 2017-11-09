8357c2612c2582fceb5ac84e84beda76fd389f35
Marius Colacioiu <marius.colacioiu@gmail.com>
2017-11-09 16:38:29 +0100

    Add smtp params

--- solution/deploy.sh
+++ solution/deploy.sh
@@ -3,5 +3,13 @@
 set -ex
 
 sam package --template-file template.yaml --s3-bucket $DEPLOYMENT_BUCKET --output-template-file packaged.yaml
-sam deploy --region eu-west-1 --template-file packaged.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_IAM
-
+sam deploy --region eu-west-1 \
+  --template-file packaged.yaml \
+  --stack-name $STACK_NAME \
+  --capabilities CAPABILITY_IAM \
+  --parameter-overrides \
+    "SmtpHost=$SMTP_HOST" \
+    "SmtpPort=$SMTP_PORT" \
+    "SmtpSenderAddress=$SMTP_SENDER_ADDRESS" \
+    "SmtpUsername=$SMTP_USERNAME" \
+    "SmtpPassword=$SMTP_PASSWORD"
