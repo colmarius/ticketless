cf9bad33bbe040e4382d7c163812d4f980446b9c
Marius Colacioiu <marius.colacioiu@gmail.com>
2017-11-09 11:38:03 +0100

    Basic mock implementation of gigs api

---
+++ lessons/04-serverless-application-model/src/index.js
@@ -0,0 +1,44 @@
+const mockGigs = [...Array(12).keys()].map(i => ({
+  slug: `band${i}-location${i}`,
+  bandName: `Mock Band ${i}`,
+  city: `Mock City ${i}`,
+  year: '1961',
+  date: '2019-01-01',
+  venue: `Mock Venue ${i}`,
+  collectionPointMap: 'map-placeholder.png',
+  collectionPoint: 'New York, NY 10001, USA',
+  collectionTime: '14:30',
+  originalDate: '1977-02-05',
+  capacity: 3000,
+  description: `Mock description ${i}`,
+  image: 'band-placeholder.png',
+  price: '1010'
+}))
+
+function findGigBySlug (slug) {
+  return mockGigs.find(gig => gig.slug === slug)
+}
+
+function response (code, body) {
+  return {
+    statusCode: code,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  }
+}
+
+exports.listGigs = (event, context, callback) => {
+  return callback(null, response(200, { gigs: mockGigs }))
+}
+
+exports.gig = (event, context, callback) => {
+  const { pathParameters: { slug } } = event
+  const gig = findGigBySlug(slug)
+  const result = gig
+    ? response(200, gig)
+    : response(404, { error: 'Gig not found' })
+  return callback(null, result)
+}
