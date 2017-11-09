81433fa637ad7501ab9ff8276f78bc0ae2e6c5d3
Marius Colacioiu <marius.colacioiu@gmail.com>
2017-11-09 10:52:18 +0100

    Add error handler to example lambda

--- lessons/03-apis-lambda/index.js
+++ lessons/03-apis-lambda/index.js
@@ -1,16 +1,30 @@
 exports.handler = (event, context, callback) => {
-  const name = event.queryStringParameters && event.queryStringParameters.name ? event.queryStringParameters.name : 'World'
-  const body = JSON.stringify({
-    message: `Hello ${name}`
-  })
+  try {
+    const name =
+      event.queryStringParameters && event.queryStringParameters.name
+        ? event.queryStringParameters.name
+        : 'World'
+    const body = JSON.stringify({
+      message: `Hello ${name}`
+    })
 
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body
+    const response = {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body
+    }
+
+    callback(null, response)
+  } catch (err) {
+    console.error(err)
+    return callback(null, {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: { error: 'Internal server error' }
+    })
   }
-
-  callback(null, response)
 }
